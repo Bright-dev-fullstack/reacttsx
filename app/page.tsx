7e543ceced80ebcc5fb2054158940d460556d0ca
Bright Iboyi <emmyblisk@gmail.com>
2026-06-22 15:11:24 +0100

    ohh

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<div className="text-blue-600">Hello world</div>
+<div className="text-blue-600 text-7xl">welcome to APIConnect...</div>
   );
 }
